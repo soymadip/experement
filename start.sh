da78c60563fd1a29cf597953e8ae420603856148
soymadip <84225810+soymadip@users.noreply.github.com>
2022-05-30 23:32:01 +0530

    cdvdf

---
+++ start.sh
@@ -0,0 +1,7 @@
+echo "Cloning Repo, Please Wait..."
+git clone https://github.com/soymadip/experement.git /EVA_Cinehub 
+cd /EVA_Cinehub 
+echo "Installing Requirements..."
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 bot.py
